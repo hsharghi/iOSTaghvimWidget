29751670f9281671fef8e0173ec6111a5cdf30b8
Hadi Sharghi <hsharghi@icloud.com>
2023-07-18 00:22:04 +0330

    Initial Commit

---
+++ Taghvim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Taghvim
+//
+//  Created by Hadi Sharghi on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Taghvim/TaghvimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaghvimApp.swift
+//  Taghvim
+//
+//  Created by Hadi Sharghi on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TaghvimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
